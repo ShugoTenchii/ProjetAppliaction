01dacea75de00b361e6d5aaa612b13e986996ceb
jerome gerard <jerome.gerard02@outlook.fr>
2022-01-24 16:57:27 +0100

    seance 17/01

--- app/src/main/java/com/jger/groupe5v2/MainActivity.java
+++ app/src/main/java/com/jger/groupe5v2/MainActivity.java
@@ -2,7 +2,10 @@ package com.jger.groupe5v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boutonCalculer = findViewById(R.id.boutonCalculer);
+        Button boutonDernierCalcul = findViewById(R.id.boutonDernierCalcul);
+        boutonCalculer.setOnClickListener(view -> lancerActivityCalcul());
+        boutonDernierCalcul.setOnClickListener(view -> lancerActivityDernierCalcul());
+
+    }
+
+    private void lancerActivityDernierCalcul() {
+        Intent intent = new Intent(this,LastComputeActivity.class);
+        startActivity(intent);
+    }
+
+    private void lancerActivityCalcul() {
+        Intent intent = new Intent(this,CalculActivity.class);
+        startActivity(intent);
     }
 }
