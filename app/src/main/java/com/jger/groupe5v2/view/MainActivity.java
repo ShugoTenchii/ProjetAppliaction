7a01675648fd2e5f85adac1af0e717fe4c2a6606
ShugoTenchii <MLKwi6057@gmail.com>
2022-03-24 20:57:40 +0100

    first commit

--- app/src/main/java/com/jger/groupe5v2/view/MainActivity.java
+++ app/src/main/java/com/jger/groupe5v2/view/MainActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.jger.groupe5v2.R;
-import com.jger.groupe5v2.view.CalculActivity;
-import com.jger.groupe5v2.view.LastComputeActivity;
+import com.jger.groupe5v2.controller.CalculService;
+import com.jger.groupe5v2.model.TypeOperationEnum;
+import com.jger.groupe5v2.model.exception.DivideException;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Integer premierElement = 0;
+    Integer deuxiemeElement = 0;
+    TypeOperationEnum typeOperation = null;
+    TextView textViewCalcul;
+    Integer BORNE_HAUTE = 9999;
+    CalculService calculService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +28,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button boutonCalculer = findViewById(R.id.button_add);
         Button boutonDernierCalcul = findViewById(R.id.button_substract);
+        Button bontonQuiz = findViewById(R.id.ButtonQuiz);
+        Button bontonResultatQuizz = findViewById(R.id.buttonResultatQuiz);
         boutonCalculer.setOnClickListener(view -> lancerActivityCalcul());
+        bontonQuiz.setOnClickListener(view -> majText());
         boutonDernierCalcul.setOnClickListener(view -> lancerActivityDernierCalcul());
+        bontonQuiz.setOnClickListener(view -> lancerActivityQuizz());
 
+
+    }
+
+
+    private void ajouterSymbol(TypeOperationEnum multiply) {
+        this.typeOperation = typeOperation;
+    }
+
+    public void ajouterNombre() {
+        premierElement = (int)(Math.random()*(9999+1));
+        deuxiemeElement = (int)(Math.random()*(9999+1));
+    }
+
+
+
+    private void majText() {
+        String textAAfficher = "";
+        ajouterNombre();
+        ajouterSymbol(TypeOperationEnum.MULTIPLY);
+        textViewCalcul = findViewById(R.id.questionCalcul);
+        if (typeOperation == null) {
+            textAAfficher = premierElement.toString();
+        } else {
+            textAAfficher = premierElement + " " + typeOperation.getSymbol() + " " + deuxiemeElement;
+        }
+        textViewCalcul.setText(textAAfficher);
     }
 
     private void lancerActivityDernierCalcul() {
@@ -32,4 +72,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CalculActivity.class);
         startActivity(intent);
     }
+
+    private void lancerActivityQuizz() {
+        Intent intent = new Intent(this, CalculActivity.class);
+        startActivity(intent);
+    }
+
+    /*private void lancerActivityResultat() {
+        Intent intent = new Intent(this, CalculActivity.class);
+        startActivity(intent);
+    }*/
+
 }
--- app/src/main/java/com/jger/groupe5v2/view/QuizzActivity.java
+++ app/src/main/java/com/jger/groupe5v2/view/QuizzActivity.java
@@ -1,7 +1,6 @@
 package com.jger.groupe5v2.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,19 +10,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jger.groupe5v2.R;
 import com.jger.groupe5v2.controller.CalculBaseHelper;
 import com.jger.groupe5v2.controller.CalculDao;
 import com.jger.groupe5v2.controller.CalculService;
 import com.jger.groupe5v2.model.Calcul;
-import com.jger.groupe5v2.model.exception.DivideException;
-import com.jger.groupe5v2.R;
 import com.jger.groupe5v2.model.TypeOperationEnum;
+import com.jger.groupe5v2.model.exception.DivideException;
 
-public class CalculActivity extends AppCompatActivity {
+public class QuizzActivity extends AppCompatActivity {
     Integer premierElement = 0;
     Integer deuxiemeElement = 0;
     TypeOperationEnum typeOperation = null;
-    TextView textViewCalcul;
+    TextView textViewCalcul = findViewById(R.id.questionCalcul);
     Integer BORNE_HAUTE = 9999;
     CalculService calculService;
 
@@ -34,67 +35,25 @@ public class CalculActivity extends AppCompatActivity {
         calculService = new CalculService(new CalculDao(new CalculBaseHelper(this)));
         textViewCalcul = findViewById(R.id.questionCalcul);
         Button button1 = findViewById(R.id.button_1_quiz);
-        button1.setOnClickListener(view -> ajouterNombre(1));
         Button button2 = findViewById(R.id.button_2_quiz);
-        button2.setOnClickListener(view -> ajouterNombre(2));
         Button button3 = findViewById(R.id.button_3_quiz);
-        button3.setOnClickListener(view -> ajouterNombre(3));
         Button button4 = findViewById(R.id.button_4_quiz);
-        button4.setOnClickListener(view -> ajouterNombre(4));
         Button button5 = findViewById(R.id.button_5_quiz);
-        button5.setOnClickListener(view -> ajouterNombre(5));
         Button button6 = findViewById(R.id.button_6_quiz);
-        button6.setOnClickListener(view -> ajouterNombre(6));
         Button button7 = findViewById(R.id.button_7_quiz);
-        button7.setOnClickListener(view -> ajouterNombre(7));
         Button button8 = findViewById(R.id.button_8_quiz);
-        button8.setOnClickListener(view -> ajouterNombre(8));
         Button button9 = findViewById(R.id.button_9_quiz);
-        button9.setOnClickListener(view -> ajouterNombre(9));
         Button button0 = findViewById(R.id.button_0_quiz);
-        button0.setOnClickListener(view -> ajouterNombre(0));
         Button boutonAdd = findViewById(R.id.button_add);
-        boutonAdd.setOnClickListener(view -> ajouterSymbol(TypeOperationEnum.ADD));
         Button boutonSubstract = findViewById(R.id.button_substract);
-        boutonSubstract.setOnClickListener(view -> ajouterSymbol(TypeOperationEnum.SUBSTRACT));
         Button boutonDivide = findViewById(R.id.button_divide);
-        boutonDivide.setOnClickListener(view -> ajouterSymbol(TypeOperationEnum.DIVIDE));
         Button boutonMultiply = findViewById(R.id.button_multiply);
-        boutonMultiply.setOnClickListener(view -> ajouterSymbol(TypeOperationEnum.MULTIPLY));
 
     }
 
-    private void ajouterSymbol(TypeOperationEnum typeOperation) {
-        this.typeOperation = typeOperation;
-        majText();
-    }
 
-    public void ajouterNombre(Integer valeur) {
-        if (typeOperation == null) {
-            if (10 * premierElement + valeur > BORNE_HAUTE) {
-                Toast.makeText(this, getString(R.string.ERROR_VALEUR_TROP_GRANDE), Toast.LENGTH_LONG).show();
-            } else {
-                premierElement = 10 * premierElement + valeur;
-            }
-        } else {
-            if (10 * deuxiemeElement + valeur > BORNE_HAUTE) {
-                Toast.makeText(this, getString(R.string.ERROR_VALEUR_TROP_GRANDE), Toast.LENGTH_LONG).show();
-            } else {
-                deuxiemeElement = 10 * deuxiemeElement + valeur;
-            }
-        }
-        majText();
-    }
 
-    private void majText() {
-        String textAAfficher = "";
-        if (typeOperation == null) {
-            textAAfficher = premierElement.toString();
-        } else {
-            textAAfficher = premierElement + " " + typeOperation.getSymbol() + " " + deuxiemeElement;
-        }
-        textViewCalcul.setText(textAAfficher);
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -118,22 +77,21 @@ public class CalculActivity extends AppCompatActivity {
     }
 
     private Integer faisLeCalcul() throws DivideException {
-            switch (typeOperation) {
-                case ADD:
-                    return premierElement + deuxiemeElement;
-                case DIVIDE:
-                    if (deuxiemeElement != 0) {
-                        return premierElement / deuxiemeElement;
-                    } else {
-                        throw new DivideException();
-                    }
+        int OperationAlea = 1 + (int)(Math.random() * ((3-1)+1));
+        if(OperationAlea == 1){
+            ajouterSymbol(TypeOperationEnum.MULTIPLY);
+            return premierElement + deuxiemeElement;
+        }else if(OperationAlea == 2){
+            ajouterSymbol(TypeOperationEnum.SUBSTRACT);
+            return premierElement - deuxiemeElement;
+        }else if(OperationAlea == 3){
+            ajouterSymbol(TypeOperationEnum.MULTIPLY);
+            return premierElement * deuxiemeElement;
+        }
+        return 0;
+    }
 
-                case SUBSTRACT:
-                    return premierElement - deuxiemeElement;
-                case MULTIPLY:
-                    return premierElement * deuxiemeElement;
-            }
-            return 0;
+    private void ajouterSymbol(TypeOperationEnum multiply) {
     }
 
     private boolean ouvrirDernierCalcul() {
